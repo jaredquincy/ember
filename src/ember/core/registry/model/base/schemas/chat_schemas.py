a25e7d36c1c3c6300ef94001f550f13558e88590
Jason Lee <jason_lee@berkeley.edu>
2025-04-04 21:54:10 -0700

    removed todo in chat_schemas.py

--- src/ember/core/registry/model/base/schemas/chat_schemas.py
+++ src/ember/core/registry/model/base/schemas/chat_schemas.py
@@ -115,6 +115,6 @@ class ChatResponse(BaseModel):
     """
 
     data: str
-    embedding: list[float] = None   # TODO: Fix embedding model structure
+    embedding: list[float] = None
     raw_output: Any = None
     usage: Optional[UsageStats] = None
